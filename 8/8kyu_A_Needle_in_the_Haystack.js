50fd03191d6e62578dc8cc1e216a8f622fb3ea63
klonirovan89 <128051429+klonirovan89@users.noreply.github.com>
2023-04-30 12:51:00 +0300

    Add files via upload

---
+++ 8/8kyu_A_Needle_in_the_Haystack.js
@@ -0,0 +1,14 @@
+function findNeedle(haystack) {
+    for (i = 0; i < haystack.length; i++){
+        if(haystack[i] == 'needle'){
+            return "found the needle at position " + i;
+        }
+    }
+}
+
+
+
+
+
+
+
